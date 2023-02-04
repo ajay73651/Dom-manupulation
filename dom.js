3b8bc7844ad910c1e4f4dd6181ce3e4287c26b10
ajay73651 <124288966+ajay73651@users.noreply.github.com>
2023-02-04 13:43:32 +0530

    second commit

--- dom.js
+++ dom.js
@@ -1,16 +1,16 @@
 //EXAMINE THE DOCMENT OBJECT
 
-// console.log(document);
+console.log(document);
 
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// // document.title = 123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[10]);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+// document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
 
 // //this is not write way to select anything from the dom: it will be change if html update
 // document.all[10].textContent = "Hello";
